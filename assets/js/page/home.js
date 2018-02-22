5255b385620e6dce348310a07ea9bbfde0a11ebc
cnzi <258345024@qq.com>
2018-02-22 16:57:32 +0800

    first commit

---
+++ assets/js/page/home.js
@@ -0,0 +1,26 @@
+var textSelect1 = document.getElementById('textSelect1');
+var p1 = document.getElementById('p1');
+textSelect1.onmouseover = function() {
+    p1.style.display = 'none';
+};
+textSelect1.onmouseout = function() {
+    p1.style.display = 'flex';
+};
+
+var textSelect2 = document.getElementById('textSelect2');
+var p2 = document.getElementById('p2');
+textSelect2.onmouseover = function() {
+    p2.style.display = 'none';
+};
+textSelect2.onmouseout = function() {
+    p2.style.display = 'flex';
+};
+
+var textSelect3 = document.getElementById('textSelect3');
+var p3 = document.getElementById('p3');
+textSelect3.onmouseover = function() {
+    p3.style.display = 'none';
+};
+textSelect3.onmouseout = function() {
+    p3.style.display = 'flex';
+};
