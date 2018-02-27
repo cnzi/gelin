b8cee2caaa6c5c4c0d012a9aacab2976efb33b79
wuwei <wuwei@bytedance.com>
2018-02-27 15:21:47 +0800

    add i18n

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,66 @@
 "use strict";
 
 var gulp = require('gulp'),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify'),
-  rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    i18n = require('gulp-html-i18n'),
+    rename = require('gulp-rename'),
     sass = require('gulp-sass'),
     maps = require('gulp-sourcemaps'),
-     del = require('del'),
-     autoprefixer = require('gulp-autoprefixer'),
-     browserSync = require('browser-sync').create(),
-     htmlreplace = require('gulp-html-replace'),
-     cssmin = require('gulp-cssmin');
+    del = require('del'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create(),
+    htmlreplace = require('gulp-html-replace'),
+    cssmin = require('gulp-cssmin');
 
-gulp.task("concatScripts", function() {
+gulp.task("concatScripts", function () {
     return gulp.src([
         'assets/js/vendor/jquery-3.2.1.slim.min.js',
         'assets/js/vendor/popper.min.js',
         'assets/js/vendor/bootstrap.min.js',
         'assets/js/functions.js'
-        ])
-    .pipe(maps.init())
-    .pipe(concat('main.js'))
-    .pipe(maps.write('./'))
-    .pipe(gulp.dest('assets/js'))
-    .pipe(browserSync.stream());
+    ])
+        .pipe(maps.init())
+        .pipe(concat('main.js'))
+        .pipe(maps.write('./'))
+        .pipe(gulp.dest('static/assets/js'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task("minifyScripts", ["concatScripts"], function() {
-  return gulp.src("assets/js/main.js")
-    .pipe(uglify())
-    .pipe(rename('main.min.js'))
-    .pipe(gulp.dest('dist/assets/js'));
+gulp.task("minifyScripts", ["concatScripts"], function () {
+    return gulp.src("static/assets/js/*.js")
+        .pipe(uglify())
+        .pipe(rename(function (path) {
+            path.extname = '.min.js';
+        }))
+        .pipe(gulp.dest('dist/assets/js'));
 });
 
-gulp.task('compileSass', function() {
-  return gulp.src(["assets/css/main.scss", "assets/css/page/*.scss"])
-      .pipe(maps.init())
-      .pipe(sass().on('error', sass.logError))
-      .pipe(autoprefixer())
-      .pipe(maps.write('./'))
-      .pipe(gulp.dest('assets/css'))
-      .pipe(browserSync.stream());
+gulp.task('compileSass', function () {
+    return gulp.src(["assets/css/main.scss", "assets/css/page/*.scss"])
+        .pipe(maps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(maps.write('./'))
+        .pipe(gulp.dest('static/assets/css'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task("minifyCss", ["compileSass"], function() {
-  return gulp.src("assets/css/main.css")
-    .pipe(cssmin())
-    .pipe(rename('main.min.css'))
-    .pipe(gulp.dest('dist/assets/css'));
+gulp.task("minifyCss", ["compileSass"], function () {
+    return gulp.src("static/assets/css/*.css")
+        .pipe(cssmin())
+        .pipe(rename(function (path) {
+            path.extname = '.min.css';
+        }))
+        .pipe(gulp.dest('dist/assets/css'));
 });
 
-gulp.task('watchFiles', function() {
-  gulp.watch('assets/css/**/*.scss', ['compileSass']);
-  gulp.watch('assets/js/*.js', ['concatScripts']);
+gulp.task('watchFiles', function () {
+    gulp.watch('assets/css/**/*.scss', ['compileSass']);
+    gulp.watch('assets/js/*.js', ['concatScripts']);
 })
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     browserSync.init({
         server: {
             baseDir: "./"
@@ -63,34 +68,49 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('clean', function() {
-  del(['dist', 'assets/css/main.css*', 'assets/js/main*.js*']);
+gulp.task('clean', function () {
+    del(['dist', 'assets/css/main.css*', 'assets/js/main*.js*']);
 });
 
-gulp.task('renameSources', function() {
-  return gulp.src('*.html')
-    .pipe(htmlreplace({
-        'js': 'assets/js/main.min.js',
-        'css': 'assets/css/main.min.css'
-    }))
-    .pipe(gulp.dest('dist/'));
+gulp.task('renameSources', function () {
+    return gulp.src('*.html')
+        .pipe(htmlreplace({
+            'js': 'assets/js/main.min.js',
+            'css': 'assets/css/main.min.css'
+        }))
+        .pipe(gulp.dest('dist/'));
 });
 
-gulp.task("build", ['minifyScripts', 'minifyCss'], function() {
-  return gulp.src(['*.html', '*.php','favicon.ico',
-                   "assets/img/**", "assets/fonts/**"], { base: './'})
-            .pipe(gulp.dest('dist'));
+gulp.task('statics', function() {
+    return gulp.src(['*.html', '*.php', 'favicon.ico',
+        "assets/img/**", "assets/fonts/**"], { base: './' })
+        .pipe(gulp.dest('static'));
 });
 
-gulp.task('serve', ['watchFiles'], function(){
-  browserSync.init({
-        server: "./"
+gulp.task("build", ['minifyScripts', 'minifyCss', 'localize'], function () {
+    return gulp.src(['*.html', 'favicon.ico', 'assets/img/*'], { base: './' })
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('localize', ['compileSass', 'concatScripts'], function () {
+    return gulp.src('*.html')
+        .pipe(i18n({
+            langDir: './lang',
+            trace: true,
+            delimiters: ['__(',')__']
+        }))
+        .pipe(gulp.dest('static'));
+});
+
+gulp.task('serve', ['watchFiles', "localize", "statics"], function () {
+    browserSync.init({
+        server: "./static/"
     });
 
     gulp.watch("assets/css/**/*.scss", ['watchFiles']);
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
-gulp.task("default", ["clean", 'build'], function() {
-  gulp.start('renameSources');
+gulp.task("default", ["clean", 'build'], function () {
+    gulp.start('renameSources');
 });
